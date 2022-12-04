4f69ee0625a5d005677816e71d8f9a07533a74e8
root <root@ip-172-31-80-58.ec2.internal>
2022-12-04 07:58:00 +0000

    script

--- automation.sh
+++ automation.sh
@@ -29,9 +29,23 @@ timestamp=$(date '+%d%y%y-%H%M%S')
 cd /var/log/apache2
 tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 
-if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]];then
-        aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-hattpd-logs-${timestamp}.tar
+if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]]; then
+        aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 fi
 
+
+docroot="/var/www/html"
+if [[ ! -f ${docroot}/inventory.html ]]; then
+        echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${docroot}/inventory.html
+fi
+
+if [[ -f ${docroot}/inventory.html ]]; then
+        size=$(du -h  /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+          echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
+fi
+
+if [[ ! -f /etc/cron.d/automation ]]; then
+        echo: "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
+fi
 
 
